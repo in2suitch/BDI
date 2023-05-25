e7a333941f5eaf27b7da9a40ac34ef21621e5f03
in2suitch <yeahsuicidewatch@yandex.ru>
2023-05-25 13:20:08 +0300

    feat: add fields and constructor

--- src/Answer.java
+++ src/Answer.java
@@ -1,2 +1,9 @@
 public class Answer {
+    private final int score;
+    private final String answerBody;
+
+    public Answer(int score, String answerBody) {
+        this.score = score;
+        this.answerBody = answerBody;
+    }
 }
