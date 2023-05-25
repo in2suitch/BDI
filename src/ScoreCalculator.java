15866aeaa6703632f3997bb62b8059224c3df7bc
in2suitch <yeahsuicidewatch@yandex.ru>
2023-05-25 17:05:48 +0300

    feat: add printResults()

--- src/ScoreCalculator.java
+++ src/ScoreCalculator.java
@@ -1,12 +1,31 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class ScoreCalculator {
-
     private int score;
-    private final Map<Integer, Answer> scoreMap = new HashMap<>();
 
-    public Map<Integer, Answer> getScoreMap() {
-        return scoreMap;
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void printResults() {
+        System.out.print("\n");
+        System.out.println("Total Score: " + score + ".");
+        System.out.print("Interpretation: ");
+
+        if (score <= 10) {
+            System.out.println("these ups and downs are considered normal.");
+        } else if (score <= 16) {
+            System.out.println("mild mood disturbance.");
+        } else if (score <= 20) {
+            System.out.println("borderline clinical depression.");
+        } else if (score <= 30) {
+            System.out.println("moderate depression.");
+        } else if (score <= 40) {
+            System.out.println("severe depression.");
+        } else {
+            System.out.println("extreme depression.");
+        }
     }
 }
