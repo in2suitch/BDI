806437a3bef3db457b43270afb18dc508a113c3a
in2suitch <yeahsuicidewatch@yandex.ru>
2023-05-25 13:21:35 +0300

    feat: add getScoreMap

--- src/ScoreCalculator.java
+++ src/ScoreCalculator.java
@@ -6,5 +6,7 @@ public class ScoreCalculator {
     private int score;
     private final Map<Integer, Answer> scoreMap = new HashMap<>();
 
-
+    public Map<Integer, Answer> getScoreMap() {
+        return scoreMap;
+    }
 }
