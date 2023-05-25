2e0733582032a9bb673fc67183383a9420eee962
in2suitch <yeahsuicidewatch@yandex.ru>
2023-05-25 13:14:57 +0300

    feat: add static Score Calculator field and saveAnswers()

--- src/BDI.java
+++ src/BDI.java
@@ -1,5 +1,10 @@
 public class BDI {
+    static ScoreCalculator scoreCalculator = new ScoreCalculator();
+
     public static void main(String[] args) {
 
+    }
+
+    private static void saveAnswers() {
     }
 }
